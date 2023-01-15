231d4712aa06b0dcf3be92589bd7b7c3467b2700
William Favaro <williamfv93@gmail.com>
2023-01-15 15:27:25 +0100

    20230115 v1.0.0: BMI_Calculator

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMI_Calculation());
+
+class BMI_Calculation extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.purple,
+      ),
+      home: InputPage(),
+    );
+  }
+}
